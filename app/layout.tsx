b932c1a9e4074723bc7ef73dcc9979aa83c622f2
net9nixya <bshzhfujwv@gmail.com>
2026-08-24 17:04:26 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
+      <head>
+        <script src="https://telegram.org/js/telegram-web-app.js" />
+      </head>
       <body>{children}</body>
     </html>
   );
